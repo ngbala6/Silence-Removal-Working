abaad09786605e984a40672b659e8d32a32f30ac
Bala Murugan N G <ngbala6@gmail.com>
2020-04-06 20:19:36 +0530

    Rearranged code Plot audio signals

--- Plot-AudioSignals/Plot_Signals.py
+++ Plot-AudioSignals/Plot_Signals.py
@@ -1,21 +1,26 @@
 from vad import VoiceActivityDetector
-import json
+# import json
 
-filename = '6TU5302374.wav'
-v = VoiceActivityDetector(filename)
-v.plot_detected_speech_regions()
+# Input file
+audiofile = "abc.wav"
 
+plotaudiosignal = VoiceActivityDetector(audiofile)
 
+# Plotting the Audio signals, so that we can identify when it is having silence in audio
 
-def save_to_file(data, file):
-    with open(file, 'w') as fp:
-        json.dump(data, fp)
-
+plotaudiosignal.plot_detected_speech_regions()
 
 
-v = VoiceActivityDetector(filename)
-raw_detection = v.detect_speech()
-print(raw_detection)
-speech_labels = v.convert_windows_to_readible_labels(raw_detection)
-
-save_to_file(speech_labels, "bala.json")
+#
+# def save_to_file(data, file):
+#     with open(file, 'w') as fp:
+#         json.dump(data, fp)
+#
+#
+#
+# v = VoiceActivityDetector(filename)
+# raw_detection = v.detect_speech()
+# print(raw_detection)
+# speech_labels = v.convert_windows_to_readible_labels(raw_detection)
+#
+# save_to_file(speech_labels, "bala.json")
